603d5f379d76820f895052c5045d985dba58cc7d
Deji Yusuf <109284424+JustDejiy@users.noreply.github.com>
2023-05-18 12:40:04 +0100

    personal portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+/*typing animation*/
+
+var typed = new Typed(".typing",{
+    strings:["","Safety Professional","Web Developer","Logistics Expert"],
+    typespeed:100,
+    Backspeed:60,
+    loop:true
+})
